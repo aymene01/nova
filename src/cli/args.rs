f937875d7650f7806fc2b99cb03e8904d19a2419
Aymene Bousbia <70906904+aymene01@users.noreply.github.com>
2025-05-12 20:15:28 +0200

    feat: start command for config generation (#5)
    * feat: init start command for config generation
    
    * style: running cargo fmt
    
    * lint: remove return key word for new config
    
    * feat: add new test for config generation
    
    * style: running cargo fmt + cargo lint

---
+++ src/cli/args.rs
@@ -0,0 +1,15 @@
+use crate::config::Config;
+use clap::Command;
+
+pub fn parse_args() -> Option<Config> {
+    let matches = Command::new("nova")
+        .about("Robot Swarm Simulation")
+        .version("0.1.0")
+        .subcommand(Command::new("start").about("Start the simulation"))
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("start", _)) => Some(Config::new()),
+        _ => None,
+    }
+}
---
+++ src/simulation/entities.rs
@@ -0,0 +1,47 @@
+use noise::Perlin;
+use std::collections::HashMap;
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum ResourceType {
+    Energy,
+    Mineral,
+    ScientificInterest,
+}
+
+#[allow(dead_code)]
+pub struct Map {
+    pub width: usize,
+    pub height: usize,
+    pub terrain: Vec<Vec<u8>>,
+    pub resources: HashMap<(usize, usize), (ResourceType, u32)>,
+    pub discovered: Vec<Vec<bool>>,
+    noise: Perlin,
+    seed: u64,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, PartialEq)]
+pub enum RobotType {
+    Explorer,
+    Harvester,
+    Scientist,
+}
+
+#[allow(dead_code)]
+pub struct Robot {
+    pub id: usize,
+    pub robot_type: RobotType,
+    pub x: usize,
+    pub y: usize,
+    pub energy: u32,
+    pub carrying: Option<(ResourceType, u32)>,
+}
+
+#[allow(dead_code)]
+pub struct Station {
+    pub resources: HashMap<ResourceType, u32>,
+    pub discoveries: u32,
+    pub x: usize,
+    pub y: usize,
+}
