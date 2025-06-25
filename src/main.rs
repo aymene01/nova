2bc36301c5839a690d2cbed7fb375618e8f6c5b5
Edofo <48387251+Edofo@users.noreply.github.com>
2025-06-25 15:33:28 +0200

    fix: format

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,9 @@ use config::Config;
 use crossterm::{
     event::{self, Event, KeyCode},
     execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
 };
-use ratatui::{backend::CrosstermBackend, Terminal};
+use ratatui::{Terminal, backend::CrosstermBackend};
 use std::io;
 use std::time::{Duration, Instant};
 
